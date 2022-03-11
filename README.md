94c1f6d3a346fcd4a3325a2e93662f6122d3c3b5
Alex Marty <64852095+AlexGMarty@users.noreply.github.com>
2022-03-11 15:15:18 -0700

    Upload

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Avrae development
- Open-source online game design
+
+Open-source online game design
+
+These aliases are used for playing Dungeons and Dragons in the online play-by-post format and are written in Drac2, a Python-based scripting language.
+
+The use of these aliases has improved the quality and ease of communication and gameplay for players.
---
+++ celerity.py
@@ -0,0 +1,38 @@
+celerity 
+embed
+<drac2>
+cc    = "Mutagencraft - Consume Mutagen"
+if not character().cc_exists(cc):
+  character().create_cc(cc,0,2,'long','bubble')
+v     = character().cc_exists(cc) and character().get_cc(cc)
+c     = combat()
+out   = []
+argList     = "@@@"
+args        = argparse(argList)
+if c:
+  target      = combat().me.name if combat().me else combat().current.name
+else:
+  err("You need to join your player into initiative before running this alias")
+ignore      = args.last('i')
+bolsterType = args.adv(custom = {'adv': 'spell', 'dis': 'bonus'})
+
+cTarget = c.get_combatant(target) if c and target else None
+
+if v or ignore:
+  cTarget.add_effect("Celerity Mutagen", "-sdis wis", desc = "Ygv's Dexterity score increases by 4, as does his Dexterity maximum.\n - Side effect: Ygv has disadvantage on Wisdom saving throws.")
+  character().mod_cc(cc, -1)
+  out.append(f""" -f "{cc} {'(-1)'*(not ignore)}|{character().cc_str(cc)}" """)
+elif not (v or ignore):
+
+  if character().cc_exists(cc):
+    out.append(f""" -f "No uses remaining!|You must finish a long rest before you can use this ability again." -f "{cc}|{character().cc_str(cc)}" """)
+  else:
+    out.append(f""" -f "Missing Counter|You appear to be missing the counter named {cc}. Please create it and try again." """)
+
+return ' '.join(out)
+</drac2>
+-title "Ygv injects his Celerity Mutagen!"
+-f "Ygv's Dexterity score increases by 4, as does his Dexterity maximum. 
+Side effect: Ygv has disadvantage on Wisdom saving throws."
+-color <color>
+-thumb "https://images-ext-1.discordapp.net/external/lE9WkjqgsYqFnlmnIn1X-oji92K7H5x5NXBzJrGLhy8/%3Fwidth%3D150%26height%3D150%26fit%3Dcrop%26quality%3D95%26auto%3Dwebp/https/www.dndbeyond.com/avatars/22129/263/1581111423-43322934.jpeg"
